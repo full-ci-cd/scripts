b8ce151092390e074644ff5fb23f3076eabda9eb
root <root@vps776263.ovh.net>
2020-01-30 22:42:41 +0100

    added options to start systemctl on centos

--- create_test_env.sh
+++ create_test_env.sh
@@ -6,10 +6,20 @@ if [[ "$1" != "-u" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
 
 fi
 
+
+##Ubuntu OPTIONS
 DOCKER_UBUNTU_RUN_CUSTOM_OPTIONS="--tmpfs=/run --tmpfs=/run/lock -v /sys/fs/cgroup/systemd:/sys/fs/cgroup/systemd --stop-signal=SIGRTMIN+3"
 DOCKER_UBUNTU_RUN_CUSTOM_COMMAND="/sbin/init"
+
+##Centos OPTIONS
+DOCKER_CENTOS_RUN_CUSTOM_OPTIONS="--privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro"
+DOCKER_CENTOS_RUN_CUSTOM_COMMAND="/usr/sbin/init"
+
+##Vars used in GETOPTS  
 NAME="null"
 CHECKER_SOFT_MODE="no"
+
+
 while getopts ":u:rs"  arg; do
 	case $arg in 
 		r)
@@ -31,16 +41,19 @@ while getopts ":u:rs"  arg; do
 	esac
 done
 
-
+##Check if containers exists
 if [[ "$(docker ps -a | grep $NAME | wc -l )" == "0" ]];then 
-## starting docker containers
-	docker run -ti --name master_a_$(echo "$NAME")  -d -p "$(shuf -i 2000-65000 -n 1 )":22 fullcicd/common_centos:0.1.4
+
+	## starting docker containers
+	docker run -ti $(echo $DOCKER_CENTOS_RUN_CUSTOM_OPTIONS) --name master_a_$(echo "$NAME")  -d -p "$(shuf -i 2000-65000 -n 1 )":22 fullcicd/common_centos:0.1.3 $DOCKER_CENTOS_RUN_CUSTOM_COMMAND
 	docker run -ti $(echo $DOCKER_UBUNTU_RUN_CUSTOM_OPTIONS) --name master_j_$(echo "$NAME")  -d -p "$(shuf -i 2000-65000 -n 1 )":22 fullcicd/common_ubuntu:0.1.3 $DOCKER_UBUNTU_RUN_CUSTOM_COMMAND
-	docker run -ti --name kube-01_$(echo "$NAME")  -d -p "$(shuf -i 2000-65000 -n 1 )":22 fullcicd/common_centos:0.1.4
-  	## applying ansible states
+	docker run -ti $(echo $DOCKER_CENTOS_RUN_CUSTOM_OPTIONS) --name kube-01_$(echo "$NAME")  -d -p "$(shuf -i 2000-65000 -n 1 )":22 fullcicd/common_centos:0.1.3 $DOCKER_CENTOS_RUN_CUSTOM_COMMAND
+
 	if [[ "$(echo $CHECKER_SOFT_MODE)" == "no" ]];then
+  		## applying ansible states
 		ansible-playbook -i ../ansible_repo/hosts_test_$(echo "$NAME") ../ansible_repo/initial_configuration_playbook -vvv
 	fi
+
 else echo "containers of user already are declared" && exit 0
 fi
 
